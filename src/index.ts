ae88e7137990498e3dadfed9117ec36dd45e2ddc
amin83th <mohammadamin83th@gmail.com>
2026-09-07 20:09:34 +0330

    feat: clear notify after 5 secend

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ import { TabPulse } from "./core/TabPulse";
 
 export const tabPulse = new TabPulse();
 
+
+setTimeout(() => {
+    tabPulse.clear();
+}, 5500);
 export { TabPulse };
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { tabPulse } from "./index";
 
 
-setTimeout(() => {
-    tabPulse.notify(1, "messages");
-    setTimeout(() => {
-        tabPulse.notify(3, "messages");
-    }, 1000);
-    setTimeout(() => {
-        tabPulse.clear();
-    }, 4000);
-}, 5000);
+// setTimeout(() => {
+//     tabPulse.notify(1, "messages");
+//     setTimeout(() => {
+//         tabPulse.notify(3, "messages");
+//     }, 1000);
+//     setTimeout(() => {
+//         tabPulse.clear();
+//     }, 4000);
+// }, 5000);
 
+
+document.querySelector('#app')?.addEventListener("click" , ()=>{
+    tabPulse.notify(3, "messages");
+})
